8a3c8fb972b72bcce64c731bb4316de0a0ef7d1b
gokuweb5 <lnavarrete1@byupathway.edu>
2026-05-16 16:10:50 -0600

    complete exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // ask for the first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // ask for the last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // blanc line 
+        Console.WriteLine();
+
+        // Print result using sign $
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
+
