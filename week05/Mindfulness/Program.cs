c743c778f1d002a0aa601c8e9e59dda93600f3cc
gokuweb5 <lnavarrete1@byupathway.edu>
2026-06-10 23:07:22 -0600

    W05 Project: Mindfulness Program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+                case "2":
+                    ReflectingActivity reflecting = new ReflectingActivity();
+                    reflecting.Run();
+                    break;
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Press enter to try again.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+    private Random _random;
+
+    public ReflectingActivity()
+    {
+        _name = "Reflecting Activity";
+        _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+        _random = new Random();
+
+        _prompts = new List<string>
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+
+        _questions = new List<string>
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+        };
+    }
+
+    public string GetRandomPrompt()
+    {
+        int index = _random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    public string GetRandomQuestion()
+    {
+        int index = _random.Next(_questions.Count);
+        return _questions[index];
+    }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine();
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+        Console.WriteLine();
+        Console.Write("When you have something in mind, press enter to continue.");
+        Console.ReadLine();
+    }
+
+    public void DisplayQuestions()
+    {
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write($"> {GetRandomQuestion()} ");
+            ShowSpinner(8);
+            Console.WriteLine();
+        }
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        DisplayPrompt();
+        DisplayQuestions();
+        DisplayEndingMessage();
+    }
+}
